a58cc0fd2aea3320e700e9b6eff5774bac5ba278
Laura Koye <laurakoye1@gmail.com>
2026-08-12 15:43:08 -0500

    Test 2 in Crypto

--- src/features/crypto/Crypto.tsx
+++ src/features/crypto/Crypto.tsx
@@ -3,7 +3,7 @@ import { Badge, Text } from "../../design-system";
 function Crypto() {
   return (
     <div className="view">
-      <Text variant="title">Testing</Text>
+      <Text variant="title">Test 2</Text>
 
       <section className="balance-card">
         <span className="balance-label">Total holdings</span>
